a714bc1a5e4fe90d00f95790cb841b82a5ba4367
tomaszachar <tomike2212@gmail.com>
2025-06-05 19:37:45 +0200

    refactor(IconSymbol): extract type, update imports

--- frontend/components/ui/IconSymbol.tsx
+++ frontend/components/ui/IconSymbol.tsx
@@ -1,21 +1,21 @@
-import React from "react";
-import { OpaqueColorValue, StyleProp, TextStyle } from "react-native";
-import { SymbolWeight } from "expo-symbols";
-import AntDesign from "@expo/vector-icons/AntDesign";
+import AntDesign from '@expo/vector-icons/AntDesign';
+import type { SymbolWeight } from 'expo-symbols';
+import type { ComponentProps, FC } from 'react';
+import type { OpaqueColorValue, StyleProp, TextStyle } from 'react-native';
 
-export type IconSymbolName = React.ComponentProps<typeof AntDesign>["name"];
+export type IconSymbolName = ComponentProps<typeof AntDesign>['name'];
 
-export function IconSymbol({
-	name,
-	size = 24,
-	color,
-	style,
-}: {
-	name: IconSymbolName;
-	size?: number;
-	color: string | OpaqueColorValue;
-	style?: StyleProp<TextStyle>;
-	weight?: SymbolWeight;
-}) {
-	return <AntDesign color={color} size={size} name={name} style={style} />;
-}
+type IconSymbolProps = {
+  name: IconSymbolName;
+  size?: number;
+  color: string | OpaqueColorValue;
+  style?: StyleProp<TextStyle>;
+  weight?: SymbolWeight;
+};
+
+export const IconSymbol: FC<IconSymbolProps> = ({
+  name,
+  size = 24,
+  color,
+  style,
+}) => <AntDesign color={color} size={size} name={name} style={style} />;
