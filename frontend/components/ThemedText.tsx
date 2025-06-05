37e05b5569bcffab433565676e2f39906bc3fb34
tomaszachar <tomike2212@gmail.com>
2025-06-05 19:36:54 +0200

    refactor(ThemedText): change to functional component and arrow function

--- frontend/components/ThemedText.tsx
+++ frontend/components/ThemedText.tsx
@@ -1,71 +1,71 @@
-import { Text, type TextProps, StyleSheet } from "react-native";
-
-import { useThemeColor } from "@/hooks/useThemeColor";
+import { useThemeColor } from '@hooks/useThemeColor';
+import type { FC } from 'react';
+import { StyleSheet, Text, type TextProps } from 'react-native';
 
 export type ThemedTextProps = TextProps & {
-	lightColor?: string;
-	darkColor?: string;
-	type?:
-		| "default"
-		| "title"
-		| "defaultSemiBold"
-		| "subtitle"
-		| "link"
-		| "description";
+  lightColor?: string;
+  darkColor?: string;
+  type?:
+    | 'default'
+    | 'title'
+    | 'defaultSemiBold'
+    | 'subtitle'
+    | 'link'
+    | 'description';
 };
 
-export function ThemedText({
-	style,
-	lightColor,
-	darkColor,
-	type = "default",
-	...rest
-}: ThemedTextProps) {
-	const color = useThemeColor({ light: lightColor, dark: darkColor }, "text");
+export const ThemedText: FC<ThemedTextProps> = ({
+  style,
+  lightColor,
+  darkColor,
+  type = 'default',
+  ...rest
+}) => {
+  const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
 
-	return (
-		<Text
-			style={[
-				{ color },
-				type === "default" ? styles.default : undefined,
-				type === "title" ? styles.title : undefined,
-				type === "defaultSemiBold" ? styles.defaultSemiBold : undefined,
-				type === "subtitle" ? styles.subtitle : undefined,
-				type === "link" ? styles.link : undefined,
-				type === "description" ? styles.description : undefined,
-				style,
-			]}
-			{...rest}
-		/>
-	);
-}
+  return (
+    <Text
+      style={[
+        { color },
+        type === 'default' ? styles.default : undefined,
+        type === 'title' ? styles.title : undefined,
+        type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
+        type === 'subtitle' ? styles.subtitle : undefined,
+        type === 'link' ? styles.link : undefined,
+        type === 'description' ? styles.description : undefined,
+        style,
+      ]}
+      {...rest}
+    />
+  );
+};
 
 const styles = StyleSheet.create({
-	default: {
-		fontSize: 16,
-		lineHeight: 24,
-	},
-	defaultSemiBold: {
-		fontSize: 16,
-		lineHeight: 24,
-		fontWeight: "600",
-	},
-	title: {
-		fontSize: 32,
-		fontWeight: "bold",
-		lineHeight: 32,
-	},
-	subtitle: {
-		fontSize: 20,
-		fontWeight: "bold",
-	},
-	link: {
-		lineHeight: 30,
-		fontSize: 16,
-		color: "#0a7ea4",
-	},
-	description: {
-		fontSize: 12,
-		lineHeight: 20,
-	},
+  default: {
+    fontSize: 16,
+    lineHeight: 24,
+  },
+  defaultSemiBold: {
+    fontSize: 16,
+    lineHeight: 24,
+    fontWeight: '600',
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    lineHeight: 32,
+  },
+  subtitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  link: {
+    lineHeight: 30,
+    fontSize: 16,
+    color: '#0a7ea4',
+  },
+  description: {
+    fontSize: 12,
+    lineHeight: 20,
+  },
 });
