3395edc0216e5ccf9973179563cfeb4a16b9ffc1
tomaszachar <tomike2212@gmail.com>
2025-06-05 20:24:08 +0200

    feat: mount warehouse routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,15 @@
+import warehouse from '@routes/warehouse';
 import { Hono } from 'hono';
 import { etag } from 'hono/etag';
 import { logger } from 'hono/logger';
 import { prettyJSON } from 'hono/pretty-json';
 
-const app = new Hono()
+const app = new Hono();
 
 app.use(etag());
 app.use(logger());
 app.use(prettyJSON({ space: 4 }));
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.route('', warehouse);
 
-export default app
+export default app;
