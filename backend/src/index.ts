c8a289379f2b553b491a71c1d9e93cf83f27a5a9
tomaszachar <tomike2212@gmail.com>
2025-06-05 20:16:24 +0200

    feat: add etag,logger and prettyJSON middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,13 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { etag } from 'hono/etag';
+import { logger } from 'hono/logger';
+import { prettyJSON } from 'hono/pretty-json';
 
 const app = new Hono()
+
+app.use(etag());
+app.use(logger());
+app.use(prettyJSON({ space: 4 }));
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
