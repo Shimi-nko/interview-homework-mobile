7b7ab37fa9bda7d8297a45c8f0e137eee33963d2
tomaszachar <tomike2212@gmail.com>
2025-06-05 19:38:47 +0200

    feat(app-layout): prevent splash screen from hiding, update useEffect and extract theme

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,37 +1,33 @@
-import { useEffect } from "react";
-import { Stack } from "expo-router";
-import { useFonts } from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
 import {
-	DarkTheme,
-	DefaultTheme,
-	ThemeProvider,
-} from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
-import { useColorScheme } from "@/hooks/useColorScheme";
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import { hideAsync, preventAutoHideAsync } from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+import { useColorScheme } from 'react-native';
+
+preventAutoHideAsync();
 
 export default function RootLayout() {
-	const colorScheme = useColorScheme();
-	const [loaded] = useFonts({
-		SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-	});
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
 
-	useEffect(() => {
-		if (loaded) {
-			SplashScreen.hideAsync();
-		}
-	}, [loaded]);
+  useEffect(() => {
+    loaded && hideAsync();
+  }, [loaded]);
 
-	if (!loaded) {
-		return null;
-	}
+  const theme = colorScheme === 'dark' ? DarkTheme : DefaultTheme;
 
-	return (
-		<ThemeProvider
-			value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-		>
-			<Stack screenOptions={{ headerShown: false }} />
-			<StatusBar style="auto" />
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider value={theme}>
+      <Stack screenOptions={{ headerShown: false }} />
+      <StatusBar style="auto" />
+    </ThemeProvider>
+  );
 }
