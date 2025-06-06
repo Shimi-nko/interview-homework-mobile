2098d7670cdb80f249c77a34ec67e874eaecde1c
tomaszachar <tomike2212@gmail.com>
2025-06-06 14:45:24 +0200

    feat(warehouse.service): create method for product creation

--- backend/src/service/warehouse.service.ts
+++ backend/src/service/warehouse.service.ts
@@ -1,6 +1,10 @@
 import { prismaClient } from '@client/prisma';
+import type { CreateProductBodyRequest } from '@models/product';
 
 export class WarehouseService {
+  static async createProduct(productData: CreateProductBodyRequest) {
+    return prismaClient.product.create({ data: productData });
+  }
   static async getProducts() {
     return prismaClient.product.findMany();
   }
