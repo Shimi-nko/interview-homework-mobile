ed60e770218f21fdf286a010b8c62eedf04a24f7
tomaszachar <tomike2212@gmail.com>
2025-06-06 14:45:57 +0200

    feat(warehouse-routes): create post url for product

--- backend/src/routes/warehouse.ts
+++ backend/src/routes/warehouse.ts
@@ -1,7 +1,15 @@
+import type { CreateProductBodyRequest } from '@models/product';
 import { WarehouseService } from '@service/warehouse.service';
 import { Hono } from 'hono';
 
 const warehouse = new Hono();
+
+warehouse.post('', async (c) => {
+  const body: CreateProductBodyRequest = await c.req.json();
+  const product = await WarehouseService.createProduct(body);
+
+  return c.json(product, 200);
+});
 
 warehouse.get('all', async (c) => {
   const products = await WarehouseService.getProducts();
