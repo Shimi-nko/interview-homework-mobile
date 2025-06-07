5bccfe5c54aba36171d81fd5021dea70626bda29
tomaszachar <tomike2212@gmail.com>
2025-06-07 18:57:03 +0200

    feat(api-client): add support for response type selection (json/text)

--- frontend/client/api-client.ts
+++ frontend/client/api-client.ts
@@ -1,11 +1,16 @@
 export const apiClient = async <T>(
   url: string,
   options?: RequestInit,
+  type: 'json' | 'text' = 'json',
 ): Promise<T> => {
   const response = await fetch(url, options);
 
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status} url: ${url}`);
+  }
+
+  if (type === 'text') {
+    return (await response.text()) as T;
   }
 
   return (await response.json()) as T;
