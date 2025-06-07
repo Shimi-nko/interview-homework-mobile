de0a2e3aa997ae36181fbfe3471dfaf7bb8971f1
tomaszachar <tomike2212@gmail.com>
2025-06-07 18:28:40 +0200

    feat(detail-screen): implement product detail screen with image and information display

---
+++ frontend/screens/detail-screen.tsx
@@ -0,0 +1,66 @@
+import { ThemedText } from '@components/ThemedText';
+import { useWarehouse } from '@context/warehouse-context';
+import { useImage } from '@hooks/use-image';
+import type { WarehouseItem } from '@models/WarehouseItem';
+import { formatCurrency } from '@utils/currency-utils';
+import { useFocusEffect, useNavigation } from 'expo-router';
+import { type FC, useCallback } from 'react';
+import { SafeAreaView, View } from 'react-native';
+
+type DetailScreenProps = {
+  productId: string;
+};
+
+export const DetailScreen: FC<DetailScreenProps> = ({ productId }) => {
+  const { data } = useWarehouse();
+  const { setOptions } = useNavigation();
+
+  const { quantity, name, price, description, imageUrl } = data?.find(
+    ({ id }) => id === productId,
+  ) as WarehouseItem;
+
+  const image = useImage(200, 200, imageUrl);
+
+  useFocusEffect(
+    useCallback(() => {
+      setOptions({
+        headerShown: true,
+        headerBackTitle: 'Back',
+        title: name,
+      });
+    }, [name, setOptions]),
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.base}>
+        {image}
+        <View style={styles.productInfo}>
+          <View style={styles.productDescription}>
+            <ThemedText type="title">{name}</ThemedText>
+            <ThemedText type="description">{description}</ThemedText>
+          </View>
+          <ThemedText type="defaultSemiBold">
+            Price: {formatCurrency(price)}
+          </ThemedText>
+          <ThemedText type="defaultSemiBold">Quantity: {quantity}</ThemedText>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  base: { padding: 16, alignItems: 'center' },
+  productInfo: {
+    gap: 8,
+    alignItems: 'center',
+  },
+  productDescription: {
+    alignItems: 'center',
+  },
+};
