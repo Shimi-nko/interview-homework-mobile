df38d987637ee55725b1da8e317e3569609568ae
tomaszachar <tomike2212@gmail.com>
2025-06-08 00:48:00 +0200

    refactor(use-form): optimize onSubmit function with useCallback for better performance

--- frontend/hooks/use-form.ts
+++ frontend/hooks/use-form.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 export const useForm = <T>(defaultValues?: T) => {
   const [formValues, setFormValues] = useState<T>(defaultValues || ({} as T));
@@ -6,7 +6,10 @@ export const useForm = <T>(defaultValues?: T) => {
   const handleChange = (name: keyof T, value: T[keyof T]) => {
     setFormValues((prev) => ({ ...prev, [name]: value }));
   };
-  const onSubmit = (callback: (values: T) => void) => callback(formValues);
+  const onSubmit = useCallback(
+    (callback: (values: T) => void) => () => callback(formValues),
+    [formValues],
+  );
 
   return { formValues, handleChange, onSubmit };
 };
