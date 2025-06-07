9d67af19db249f2d3c5cc110bf8e7a5b4ab04a97
tomaszachar <tomike2212@gmail.com>
2025-06-07 19:18:46 +0200

    feat(use-image): simplify image loading and add default placeholder

--- frontend/hooks/use-image.tsx
+++ frontend/hooks/use-image.tsx
@@ -1,15 +1,7 @@
-import { useThemeColor } from '@hooks/useThemeColor';
-import { Fragment, useMemo, useState } from 'react';
-import {
-  ActivityIndicator,
-  Image,
-  type ImageSourcePropType,
-} from 'react-native';
+import { useMemo } from 'react';
+import { Image, type ImageSourcePropType } from 'react-native';
 
 export const useImage = (width: number, height: number, uri?: string) => {
-  const [loading, setLoading] = useState(true);
-  const color = useThemeColor({}, 'icon');
-
   const imageSource: ImageSourcePropType = useMemo(() => {
     return {
       uri,
@@ -23,24 +15,14 @@ export const useImage = (width: number, height: number, uri?: string) => {
     return null;
   }
 
-  const onLoadStart = () => {
-    setLoading(true);
-  };
-
-  const onLoadEnd = () => {
-    setLoading(false);
-  };
-
   return (
-    <Fragment>
-      {loading && <ActivityIndicator size="small" color={color} />}
-      <Image
-        style={{ width, height }}
-        source={imageSource}
-        onLoadStart={onLoadStart}
-        onLoad={onLoadEnd}
-        onLoadEnd={onLoadEnd}
-      />
-    </Fragment>
+    <Image
+      defaultSource={{
+        uri: require('@assets/images/image-placeholder.png'),
+        height,
+        width,
+      }}
+      source={imageSource}
+    />
   );
 };
