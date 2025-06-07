33fdc759cd662e2f776e095d1527cc7ef9423adc
tomaszachar <tomike2212@gmail.com>
2025-06-07 19:45:27 +0200

    feat(add-button): implement AddButton component for navigation to add screen

---
+++ frontend/components/ui/add-button.tsx
@@ -0,0 +1,36 @@
+import { IconSymbol } from '@components/ui/IconSymbol';
+import { useRouter } from 'expo-router';
+import type { FC } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+export const AddButton: FC = () => {
+  const { bottom } = useSafeAreaInsets();
+  const { push } = useRouter();
+
+  const style = styles(bottom);
+
+  const onPress = () => push('/add');
+
+  return (
+    <TouchableOpacity
+      activeOpacity={0.5}
+      style={style.addButton}
+      onPress={onPress}
+    >
+      <IconSymbol name="pluscircle" color="white" />
+    </TouchableOpacity>
+  );
+};
+
+const styles = (bottomOffset: number) => ({
+  addButton: {
+    position: 'absolute',
+    bottom: bottomOffset,
+    right: 16,
+    backgroundColor: '#007bff',
+    borderRadius: 99,
+    padding: 16,
+    alignItems: 'center',
+  },
+});
