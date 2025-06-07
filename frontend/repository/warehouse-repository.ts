953abc200739e13fcfb84c5709101f0cc212468d
tomaszachar <tomike2212@gmail.com>
2025-06-07 18:57:20 +0200

    feat(warehouse-repository): add deleteProductById function for product removal

--- frontend/repository/warehouse-repository.ts
+++ frontend/repository/warehouse-repository.ts
@@ -1,7 +1,11 @@
 import { apiClient } from '@client/api-client';
-import { ALL_PRODUCTS_URL } from '@constants/network';
+import { ALL_PRODUCTS_URL, PRODUCT_BY_ID_URL } from '@constants/network';
 import type { WarehouseItem } from '@models/WarehouseItem';
 
-export const getAllWarehouseProducts = async () => {
-  return apiClient<WarehouseItem[]>(ALL_PRODUCTS_URL);
+export const getAllWarehouseProducts = async () =>
+  apiClient<WarehouseItem[]>(ALL_PRODUCTS_URL);
+
+export const deleteProductById = async (id: string) => {
+  const url = PRODUCT_BY_ID_URL(id);
+  return apiClient<string>(url, { method: 'DELETE' }, 'text');
 };
