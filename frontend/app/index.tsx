88cd17275d0e2dd066f68f3c0dae71f7f3435932
tomaszachar <tomike2212@gmail.com>
2025-06-07 14:28:14 +0200

    feat(index): replace index component with landing screen

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,32 +1,3 @@
-import { ProductListing } from '@components/ProductListing';
-import {} from '@components/ui/IconSymbol';
-import { useWarehouse } from '@context/warehouse-context';
-import type { WarehouseItem } from '@models/WarehouseItem';
-import { useCallback } from 'react';
-import { FlatList, SafeAreaView, StyleSheet } from 'react-native';
+import { LandingScreen } from '@screens/landing-screen';
 
-export default function Index() {
-  const { data } = useWarehouse();
-
-  const renderItem = useCallback(
-    ({ item }: { item: WarehouseItem }) => <ProductListing item={item} />,
-    [],
-  );
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={data || []}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={renderItem}
-      />
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-  },
-});
+export default LandingScreen;
