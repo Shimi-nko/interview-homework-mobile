b63bbf871feea24e188259c3516751dafa18134c
tomaszachar <tomike2212@gmail.com>
2025-06-08 15:55:14 +0200

    refactor(ThemedText): update import path for useThemeColor and simplify component structure

--- frontend/components/ThemedText.tsx
+++ frontend/components/ThemedText.tsx
@@ -1,17 +1,12 @@
-import { useThemeColor } from '@hooks/useThemeColor';
+import { useThemeColor } from '@hooks/use-theme-color';
+import { Typography, type TypographyType } from '@theme/typography';
 import type { FC } from 'react';
-import { StyleSheet, Text, type TextProps } from 'react-native';
+import { Text, type TextProps } from 'react-native';
 
 export type ThemedTextProps = TextProps & {
   lightColor?: string;
   darkColor?: string;
-  type?:
-    | 'default'
-    | 'title'
-    | 'defaultSemiBold'
-    | 'subtitle'
-    | 'link'
-    | 'description';
+  type?: TypographyType;
 };
 
 export const ThemedText: FC<ThemedTextProps> = ({
@@ -23,49 +18,5 @@ export const ThemedText: FC<ThemedTextProps> = ({
 }) => {
   const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
 
-  return (
-    <Text
-      style={[
-        { color },
-        type === 'default' ? styles.default : undefined,
-        type === 'title' ? styles.title : undefined,
-        type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
-        type === 'subtitle' ? styles.subtitle : undefined,
-        type === 'link' ? styles.link : undefined,
-        type === 'description' ? styles.description : undefined,
-        style,
-      ]}
-      {...rest}
-    />
-  );
+  return <Text style={[{ color, ...Typography[type] }, style]} {...rest} />;
 };
-
-const styles = StyleSheet.create({
-  default: {
-    fontSize: 16,
-    lineHeight: 24,
-  },
-  defaultSemiBold: {
-    fontSize: 16,
-    lineHeight: 24,
-    fontWeight: '600',
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    lineHeight: 32,
-  },
-  subtitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  link: {
-    lineHeight: 30,
-    fontSize: 16,
-    color: '#0a7ea4',
-  },
-  description: {
-    fontSize: 12,
-    lineHeight: 20,
-  },
-});
