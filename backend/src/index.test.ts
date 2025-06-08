b8b8d52b4eaa2c2b9930f2694934be78bc57e442
tomaszachar <tomike2212@gmail.com>
2025-06-08 22:06:22 +0200

    test: improve test naming and wrap update tests in describe

--- backend/src/index.test.ts
+++ backend/src/index.test.ts
@@ -62,7 +62,7 @@ describe('root endpoint - create', () => {
     expect(await res.json()).toMatchObject(product);
   });
 
-  it('shouldn`t create product if there are non supported values in body', async () => {
+  it('should throw error and return 400 status, if there are unsupported values in body', async () => {
     const product = {
       ...MOCK_DATA[0],
       notSupportedValue: 'something something',
@@ -76,44 +76,46 @@ describe('root endpoint - create', () => {
   });
 });
 
-it('root endpoint - update', async () => {
-  const createProductRes = await app.request('', {
-    method: 'POST',
-    body: JSON.stringify(MOCK_DATA[0]),
+describe('root endpoint - update', () => {
+  it('root endpoint - update', async () => {
+    const createProductRes = await app.request('', {
+      method: 'POST',
+      body: JSON.stringify(MOCK_DATA[0]),
+    });
+
+    const { id }: Product = await createProductRes.json();
+
+    const updateProduct = { ...MOCK_DATA[0], price: 1000 };
+
+    const res = await app.request(`${id}`, {
+      method: 'PATCH',
+      body: JSON.stringify(updateProduct),
+    });
+
+    expect(res.status).toBe(200);
+    const responseJSON = await res.json();
+
+    expect(responseJSON.price).toEqual(updateProduct.price);
   });
 
-  const { id }: Product = await createProductRes.json();
+  it('should throw error and return 400 status, if there are unsupported values in body', async () => {
+    const createProductRes = await app.request('', {
+      method: 'POST',
+      body: JSON.stringify(MOCK_DATA[0]),
+    });
 
-  const updateProduct = { ...MOCK_DATA[0], price: 1000 };
+    const { id, createdAt, updatedAt, ...rest }: Product =
+      await createProductRes.json();
 
-  const res = await app.request(`${id}`, {
-    method: 'PATCH',
-    body: JSON.stringify(updateProduct),
+    const updateProduct = { ...rest, notSupportedValue: 'something something' };
+
+    const res = await app.request(`${id}`, {
+      method: 'PATCH',
+      body: JSON.stringify(updateProduct),
+    });
+
+    expect(res.status).toBe(400);
   });
-
-  expect(res.status).toBe(200);
-  const responseJSON = await res.json();
-
-  expect(responseJSON.price).toEqual(updateProduct.price);
-});
-
-it('shouldn`t update product if there are non supported values in body', async () => {
-  const createProductRes = await app.request('', {
-    method: 'POST',
-    body: JSON.stringify(MOCK_DATA[0]),
-  });
-
-  const { id, createdAt, updatedAt, ...rest }: Product =
-    await createProductRes.json();
-
-  const updateProduct = { ...rest, notSupportedValue: 'something something' };
-
-  const res = await app.request(`${id}`, {
-    method: 'PATCH',
-    body: JSON.stringify(updateProduct),
-  });
-
-  expect(res.status).toBe(400);
 });
 
 describe('root endpoint - delete', () => {
@@ -134,7 +136,7 @@ describe('root endpoint - delete', () => {
     expect(await res.text()).toBe(id);
   });
 
-  it('shouldn`t delete product if there are non supported values in body', async () => {
+  it('should throw error and return 400 status, if there are unsupported values in body', async () => {
     const res = await app.request('/unique-id-123', {
       method: 'DELETE',
     });
