933f45212487172d26056e9b14b3bdc145844aa9
tomaszachar <tomike2212@gmail.com>
2025-06-08 13:17:19 +0200

    feat(warehouse-error): add WarehouseError component for handling warehouse errors

---
+++ frontend/components/warehouse/warehouse-error.tsx
@@ -0,0 +1,41 @@
+import { ThemedText } from '@components/ThemedText';
+import { IconSymbol } from '@components/ui/IconSymbol';
+import { Button } from '@components/ui/button';
+import { useWarehouse } from '@context/warehouse-context';
+import type { FC } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+export const WarehouseError: FC = () => {
+  const { refetch, loading } = useWarehouse();
+  return (
+    <View style={styles.container}>
+      <View style={styles.errorContainer}>
+        <IconSymbol name="exclamationcircleo" color="red" size={100} />
+        <ThemedText type="subtitle" style={styles.subtitle}>
+          There was error receiving products from warehouse
+        </ThemedText>
+      </View>
+      <Button
+        title="Retry"
+        onPress={refetch}
+        style={styles.button}
+        disabled={loading}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, paddingTop: 16, gap: 24 },
+  subtitle: { textAlign: 'center' },
+  errorContainer: {
+    gap: 16,
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#4CAF50',
+    padding: 24,
+    borderRadius: 12,
+    alignItems: 'center',
+  },
+});
