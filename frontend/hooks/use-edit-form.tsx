e2d07a97cd126ed687631b34cd6213300a19a103
tomaszachar <tomike2212@gmail.com>
2025-06-08 11:41:15 +0200

    feat(edit-form): create custom hook for managing edit form state and validation

---
+++ frontend/hooks/use-edit-form.tsx
@@ -0,0 +1,76 @@
+import type { FormInputProps } from '@components/ui/form-input';
+import { useForm } from '@hooks/use-form';
+import type { CreateWarehouseItem } from '@models/WarehouseItem';
+import { useCallback, useMemo } from 'react';
+
+export const useEditForm = (product?: CreateWarehouseItem) => {
+  const { formValues, handleChange, onSubmit } =
+    useForm<CreateWarehouseItem>(product);
+
+  const handleTextChange = useCallback(
+    (target: keyof CreateWarehouseItem, value: string) => {
+      handleChange(target, value);
+    },
+    [handleChange],
+  );
+
+  const handleNumberChange = useCallback(
+    (target: keyof CreateWarehouseItem, value: string) => {
+      const numberValue = value.replace(',', '.');
+      handleChange(target, Number(numberValue));
+    },
+    [handleChange],
+  );
+
+  const disabledSubmit = useMemo(() => {
+    const requiredFields =
+      !formValues.name ||
+      !formValues.description ||
+      !formValues.price ||
+      !formValues.quantity;
+
+    const minimalQuantity = formValues.quantity < 1;
+    const minimalPrice = formValues.price < 0;
+
+    return requiredFields || minimalQuantity || minimalPrice;
+  }, [formValues]);
+
+  const data: FormInputProps[] = useMemo(
+    () => [
+      {
+        label: 'Product name',
+        placeholder: 'Super Product',
+        defaultValue: product?.name,
+        onChangeText: (value) => handleTextChange('name', value),
+      },
+      {
+        label: 'Product description',
+        multiline: true,
+        defaultValue: product?.description,
+        placeholder: 'This is a great product that does many things.',
+        onChangeText: (value) => handleTextChange('description', value),
+      },
+      {
+        label: 'Unit price(in €)',
+        placeholder: '1,5',
+        defaultValue: String(product?.price),
+        keyboardType: 'decimal-pad',
+        onChangeText: (value) => handleNumberChange('price', value),
+      },
+      {
+        label: 'Initial quantity',
+        placeholder: '10',
+        defaultValue: String(product?.quantity),
+        keyboardType: 'number-pad',
+        onChangeText: (value) => handleNumberChange('quantity', value),
+      },
+      {
+        label: 'Image url (optional)',
+        defaultValue: product?.imageUrl,
+        onChangeText: (value) => handleTextChange('imageUrl', value),
+      },
+    ],
+    [handleTextChange, handleNumberChange, product],
+  );
+  return { data, disabledSubmit, onSubmit };
+};
