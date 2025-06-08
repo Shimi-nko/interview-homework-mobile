6e2f2f830270b1a336244af9c2f6a0df83b31ce7
tomaszachar <tomike2212@gmail.com>
2025-06-08 17:54:09 +0200

    fix(warehouse-route): add error handling for delete and update operations

--- backend/src/routes/warehouse.ts
+++ backend/src/routes/warehouse.ts
@@ -4,6 +4,8 @@ import type {
 } from '@models/product';
 import { WarehouseService } from '@service/warehouse.service';
 import { Hono } from 'hono';
+import { HTTPException } from 'hono/http-exception';
+import { Prisma } from 'prisma';
 
 const warehouse = new Hono();
 
@@ -20,20 +22,34 @@ warehouse.get('all', async (c) => {
   return c.json(products, 200);
 });
 
-warehouse.delete(':id', async (c) => {
+warehouse.delete(':id', async (c, next) => {
   const id = c.req.param('id');
-  const deletedProduct = await WarehouseService.deleteProductById(id);
+  try {
+    const deletedProduct = await WarehouseService.deleteProductById(id);
 
-  return c.text(deletedProduct.id, 200);
+    return c.text(deletedProduct.id, 200);
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      throw new HTTPException(400, { message: `Record for ${id} not found` });
+    }
+    await next();
+  }
 });
 
-warehouse.patch(':id', async (c) => {
+warehouse.patch(':id', async (c, next) => {
   const id = c.req.param('id');
   const body: UpdateProductBodyRequest = await c.req.json();
 
-  const product = await WarehouseService.updateProductById(id, body);
+  try {
+    const product = await WarehouseService.updateProductById(id, body);
 
-  return c.json(product, 200);
+    return c.json(product, 200);
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientValidationError) {
+      throw new HTTPException(400, { message: error.message });
+    }
+    await next();
+  }
 });
 
 export default warehouse;
