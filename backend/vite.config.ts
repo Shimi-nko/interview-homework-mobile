9eb2ac765fc6fd07e7a2cb1c303eacfbcf72bc1b
tomaszachar <tomike2212@gmail.com>
2025-06-08 19:01:06 +0200

    build: add Vite configuration with tsconfig paths support

---
+++ backend/vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig, loadEnv } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  test: {
+    env: loadEnv('test', '', ''),
+  },
+  plugins: [tsconfigPaths()],
+});
