2ee4014ca1beb4ff10cad22702bdd5b57b0b72f9
tomaszachar <tomike2212@gmail.com>
2025-06-09 17:03:33 +0200

    feat(add-screen): update keyboard avoiding behavior for Android platform

--- frontend/screens/add-screen.tsx
+++ frontend/screens/add-screen.tsx
@@ -55,7 +55,7 @@ export const AddScreen: FC = () => {
 
   return (
     <KeyboardAvoidingView
-      behavior={Platform.select({ ios: 'padding', android: 'height' })}
+      behavior={Platform.select({ ios: 'padding', android: undefined })}
       style={styles.keyboardAvoidingView}
     >
       <SafeAreaView style={styles.container}>
