55254cbb737f5577494720060cea96d7eea9c0cf
tomaszachar <tomike2212@gmail.com>
2025-06-09 17:07:19 +0200

    feat(string-utils): add function to replace comma with period symbol

--- frontend/utils/string-utils.ts
+++ frontend/utils/string-utils.ts
@@ -1,2 +1,6 @@
 export const removeAccents = (str: string) =>
   str.normalize('NFD').replace(/\p{Diacritic}/gu, '');
+
+export const replaceCommaForPeriodSymbol = (input: string) => {
+  return input.replace(',', '.');
+};
